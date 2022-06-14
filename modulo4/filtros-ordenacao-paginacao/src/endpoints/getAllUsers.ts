5ff6cc7c003bd52a4108d83727020d2f9960c619
Guilherme Lira <Guilhermealira@gmail.com>
2022-06-14 16:31:51 -0300

    Primeira parte

---
+++ modulo4/filtros-ordenacao-paginacao/src/endpoints/getAllUsers.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express"
+import { connection } from "../data/connection"
+import { user } from "../types"
+
+export default async function selectAllUsers():Promise<any> {
+    const result = await connection.raw(`
+       SELECT id, name, email, type FROM aula49_exercicio;
+    `)
+ 
+    return result[0]
+}
+
+export const getAllUsers = async(req: Request,res: Response): Promise<void> =>{
+    try {
+       const users = await selectAllUsers()
+ 
+       if(!users.length){
+          res.statusCode = 404
+          throw new Error("No users found")
+       }
+ 
+       res.status(200).send(users)
+       
+    } catch (error:any) {
+       console.log(error)
+       res.send(error.message || error.sqlMessage)
+    }
+}
