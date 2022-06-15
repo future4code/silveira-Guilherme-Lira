93fa46aedc44e9a0ed393b2d80fa2ec01963533f
Guilherme Lira <Guilhermealira@gmail.com>
2022-06-15 09:04:18 -0300

    Mais questões feitas

--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { AddressInfo } from "net"
 import { getAllUsers } from "./endpoints/getAllUsers"
+import { getUserByType } from './endpoints/getUserByType'
+import { getUserOrder } from './endpoints/getUserOrder'
 
 export const app = express()
 
@@ -9,6 +11,8 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/users", getAllUsers)
+app.get("/users:type", getUserByType)
+app.get("/users/params", getUserOrder)
 
 const server = app.listen(process.env.PORT || 3003, () => {
    if (server) {
