6c12361493622ea0feb4fce3d62a2d4d6d569fc6
Guilherme Lira <Guilhermealira@gmail.com>
2022-07-20 19:43:34 -0300

    Primeira parte - LabeFood

---
+++ modulo6/rodada-cases-semana(1)/src/Routes/coordinator.js
@@ -0,0 +1,15 @@
+export const goToLogin = (navigate) => {
+    navigate('/')
+}
+
+export const goToFeed = (navigate) => {
+    navigate('/feed')
+}
+
+export const goToSignUp = (navigate) => {
+    navigate('/signup')
+}
+
+export const goToSignUpAddress = (navigate) => {
+    navigate('/signup/address')
+}
