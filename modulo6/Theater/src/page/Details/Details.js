d0137bdeb1f78e8c4abc3114528b63278aa7f8cd
Guilhermearrud <Guilhermealira@gmail.com>
2022-09-09 13:57:23 -0300

    Atualizado

--- modulo6/Theater/src/page/Details/Details.js
+++ modulo6/Theater/src/page/Details/Details.js
@@ -4,7 +4,7 @@ import { Main } from "./style"
 import { useParams } from "react-router-dom"
 import { useState } from 'react';
 import axios from "axios";
-import { API_KEY, BASE_URL } from './../../Contants/url';
+import { API_KEY, BASE_URL, IMG_URL } from './../../Contants/url';
 import { useEffect } from 'react';
 
 export const Details = (props) => {
@@ -16,8 +16,8 @@ export const Details = (props) => {
     const getMoviesDetails = () => {
         axios.get(`${BASE_URL}/movie/${params.id}${API_KEY}`)
             .then((res) => {
-                console.log(res.data)
-                setMovieDetails(res.data)
+                console.log([res.data])
+                setMovieDetails([res.data])
                 console.log('taqui')
                 console.log(movieDetails)
             })
@@ -29,7 +29,22 @@ export const Details = (props) => {
     const movieDetailsList = movieDetails.map((movie) => {
         return (
             <div key={movie.id}>
-                <p> {movie.original_title} </p>
+                <div>
+                    <img src={`${IMG_URL}${movie.poster_path}`} alt=""/>
+                </div>
+                <div>
+                    <strong>{movie.title}</strong>
+                    <h3>{movie.release_date}</h3>
+                    <div>
+                        <span>{movie.vote_average}</span>
+                    </div>
+                    <p>{movie.overview}</p>
+
+                    <button>
+                        <h2>Trailer</h2>
+                        
+                    </button>
+                </div>
             </div>
         )
     })
@@ -47,30 +62,6 @@ export const Details = (props) => {
             <div>
                 {movieDetailsList}
             </div>
-
-            {/* <Main>
-                <Poster>
-                    <img
-                        src={`${process.env.REACT_APP_BASE_URL_IMAGEM}${movieSelected?.poster_path}`}
-                        alt=""
-                    />
-                </Poster>
-                <DetailMovie>
-                    <strong>{movieSelected?.title}</strong>
-                    <h3>{releaseDate}</h3>
-                    <div>
-                        <AiFillStar color="#f4f006" size={25} />
-                        <span>{movieSelected?.vote_average}</span>
-                    </div>
-                    <p>{movieSelected?.overview}</p>
-
-                    <button>
-                        <h2>Trailer</h2>
-                        <AiFillPlayCircle color="#b71313" size={50} />
-                    </button>
-                </DetailMovie>
-            </Main> */}
-
-        </div >
+        </div>
     )
 }
